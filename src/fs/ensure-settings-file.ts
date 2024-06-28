667fafe2765f7633814ab04750ac8db70208edf4
Madeline <madeline@rykan.net>
2024-06-29 00:16:20 +0100

    Update to support tauri v2

--- src/fs/ensure-settings-file.ts
+++ src/fs/ensure-settings-file.ts
@@ -1,5 +1,5 @@
-import { appConfigDir, join } from '@tauri-apps/api/path';
-import { createDir, readDir, readTextFile, writeFile } from '@tauri-apps/api/fs';
+import { BaseDirectory, appConfigDir, join } from '@tauri-apps/api/path';
+import { mkdir, readDir, readTextFile, writeFile } from '@tauri-apps/plugin-fs';
 
 import { ConfigOptions, parseOptions } from '../config/config';
 
@@ -7,68 +7,67 @@ import { ConfigOptions, parseOptions } from '../config/config';
  * @internal
  */
 export enum STATUS {
-  FILE_EXISTS = 'file_exists',
-  FILE_CREATED = 'file_created'
+	FILE_EXISTS = 'file_exists',
+	FILE_CREATED = 'file_created'
 }
 
 /**
  * @internal
  */
 export async function ensureSettingsFile(options: ConfigOptions = {}): Promise<{
-  status: STATUS,
-  path: string,
-  content: string,
+	status: STATUS,
+	path: string,
+	content: string,
 }> {
-  try {
-    const finalConfig = parseOptions(options);
-    const finalDir = finalConfig.dir ?? await appConfigDir();
+	try {
+		const finalConfig = parseOptions(options);
+		const finalDir = finalConfig.dir ?? await appConfigDir();
 
-    const settingsFilePath = await join(finalDir, finalConfig.fileName);
+		const settingsFilePath = await join(finalDir, finalConfig.fileName);
 
-    // create appConfigDir()
-    try {
-      await readDir(finalDir);
-    }
-    catch (e) {
-      // doesn't exist
-      try {
-        await createDir(finalDir, {recursive: true});
-      }
-      catch (e) {
-        throw e;
-      }
-    }
+		// create appConfigDir()
+		try {
+			await readDir(finalDir);
+		}
+		catch (e) {
+			// doesn't exist
+			try {
+				await mkdir(finalDir, { recursive: true });
+			}
+			catch (e) {
+				throw e;
+			}
+		}
 
-    try {
-      const content = await readTextFile(settingsFilePath);
+		try {
+			const content = await readTextFile(settingsFilePath);
 
-      return {
-        status: STATUS.FILE_EXISTS,
-        path: settingsFilePath,
-        content
-      }
-    }
-    catch(e) {
-      // doesn't exist
+			return {
+				status: STATUS.FILE_EXISTS,
+				path: settingsFilePath,
+				content
+			};
+		}
+		catch (e) {
+			// doesn't exist
 
-        try {
-        await writeFile({
-          contents: JSON.stringify({}, null, finalConfig.prettify ? finalConfig.numSpaces : 0),
-          path: settingsFilePath
-        })
+			try {
+				const enc = new TextEncoder();
 
-        return {
-          status: STATUS.FILE_CREATED,
-          path: settingsFilePath,
-          content: JSON.stringify({}, null, finalConfig.prettify ? finalConfig.numSpaces : 0)
-        }
-      }
-      catch (e) {
-        throw e;
-      }
-    }
-  }
-  catch (e) {
-    throw e;
-  }
+				await writeFile(settingsFilePath, enc.encode(JSON.stringify({}, null, finalConfig.prettify ? finalConfig.numSpaces : 0)), { baseDir: BaseDirectory.AppConfig });
+
+				return {
+					status: STATUS.FILE_CREATED,
+					path: settingsFilePath,
+					content: JSON.stringify({}, null, finalConfig.prettify ? finalConfig.numSpaces : 0)
+				};
+			}
+			catch (e) {
+				throw e;
+			}
+		}
+	}
+	catch (e) {
+		throw e;
+	}
 }
--- src/fs/load-save.ts
+++ src/fs/load-save.ts
@@ -1,25 +1,23 @@
 import { ensureSettingsFile, STATUS } from './ensure-settings-file';
-import { writeFile } from '@tauri-apps/api/fs';
+import { BaseDirectory, writeFile } from '@tauri-apps/plugin-fs';
 import { ConfigOptions, parseOptions } from '../config/config';
 
 /**
  * @internal
  */
 export async function saveSettings
-  <SettingsSchema extends any>
-  (newSettings: SettingsSchema, path: string, options: ConfigOptions)
-{
-  try {
-    const finalConfig = parseOptions(options);
+	<SettingsSchema extends any>
+	(newSettings: SettingsSchema, path: string, options: ConfigOptions) {
+	try {
+		const finalConfig = parseOptions(options);
 
-    return await writeFile({
-      contents: JSON.stringify(newSettings, null, finalConfig.prettify ? finalConfig.numSpaces : 0),
-      path
-    })
-  }
-  catch (e) {
-    throw e;
-  }
+		const enc = new TextEncoder();
+
+		return await writeFile(path, enc.encode(JSON.stringify(newSettings, null, finalConfig.prettify ? finalConfig.numSpaces : 0)), { baseDir: BaseDirectory.AppConfig });
+	}
+	catch (e) {
+		throw e;
+	}
 }
 
 
@@ -28,19 +26,18 @@ export async function saveSettings
  * @returns The entire settings object.
  */
 export async function getSettings
-  <SettingsSchema extends any>
-  (options: ConfigOptions = {}): Promise<{settings: SettingsSchema, path: string, status: STATUS}>
-{
-  try {
-    const settingsFile = await ensureSettingsFile(options);
+	<SettingsSchema extends any>
+	(options: ConfigOptions = {}): Promise<{ settings: SettingsSchema, path: string, status: STATUS; }> {
+	try {
+		const settingsFile = await ensureSettingsFile(options);
 
-    return {
-      settings: JSON.parse(settingsFile.content) as SettingsSchema,
-      path: settingsFile.path,
-      status: settingsFile.status
-    }
-  }
-  catch (e) {
-    throw e;
-  }
+		return {
+			settings: JSON.parse(settingsFile.content) as SettingsSchema,
+			path: settingsFile.path,
+			status: settingsFile.status
+		};
+	}
+	catch (e) {
+		throw e;
+	}
 }
